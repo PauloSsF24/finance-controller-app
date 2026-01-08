8161bd013dddc9c80e5977fb23b4624014f364a4
Paulo <pssf23tc@gmail.com>
2026-01-08 19:59:05 -0300

    Subindo os controladores salvar as viagens

---
+++ controllers/viagem_controller.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from models.viagem_model import inserir_viagem
+from models.database import get_db_connection
+
+def salvar_viagem(dados):
+    inserir_viagem(
+        dados['data'],
+        dados['origem'],
+        dados['destino'],
+        dados['frete'],
+        dados['diesel'],
+        dados['pedagio'],
+        dados['manutencao'],
+        dados['outros']
+    )
+
+def obter_dashboard():
+    conn = get_connection()
+    df = pd.read_sql("SELECT * FROM viagens", conn)
+    conn.close()
+    return df
